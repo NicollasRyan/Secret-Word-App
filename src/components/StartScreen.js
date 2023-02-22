4859441310cb919192226d0c97c70dee8bf36a14
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-02-22 12:20:19 -0300

    Finalizando tela inicial

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import "./StartScreen.css";
+
+export function StartScreen() {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão para iniciar o jogo!</p>
+      <button>Começar jogar</button>
+    </div>
+  );
+}
