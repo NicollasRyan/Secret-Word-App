90708239d6a232e98acb8a31c98da7fc5ffcbb84
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-02-22 12:05:08 -0300

    linpando app

---
+++ src/data/words.js
@@ -0,0 +1,6 @@
+export const wordsLits = {
+  carro: ["Motor", "Porta", "Volante", "Peneu"],
+  Fruta: ["Banana", "Maçã", "Manga", "Laranja"],
+  computador: ["Mouse", "Tecaldo", "Monitor"],
+  Alimento: ["Arroz", "Feijão", "Carne", "Ovo"],
+};
